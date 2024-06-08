360ed94797f9736824139770b3b58c23983776b4
DAGHHHH <jabrane.jelah@yahoo.com>
2024-06-08 19:33:01 +0100

    Message

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,12 +1,14 @@
 import { NextResponse } from "next/server";
-import db from '@/lib/db'; 
+import db from '@/lib/db';
 import { message } from "antd";
+import { hash } from 'bcrypt'
+import Password from "antd/es/input/Password";
 
 export async function POST(req: Request) {
     try {
         const body = await req.json();
 
-        const { email, username } = body
+        const { email, username, password } = body
         const existingUserByEmail = await db.user.findUnique({
             where: { email: email }
         });
@@ -20,8 +22,20 @@ export async function POST(req: Request) {
             return NextResponse.json({ user: null, message: "This username Already in our Database !" })
         }
 
+        const HashedPassword = await hash(password, 10);
 
-        return NextResponse.json(body)
+        const creatingUser = await db.user.create({
+            data: {
+                email,
+                username,
+                password: HashedPassword 
+
+            }
+        });
+
+
+
+        return NextResponse.json({ user: creatingUser, message: "user created succusfully" }, { status: 201 })
 
     } catch (error) {
 
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,15 @@
-// @/lib/db/index.ts
+import { PrismaClient } from '@prisma/client'
 
-import { PrismaClient } from '@prisma/client';
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-// Initialize the Prisma client instance
-export const prisma = new PrismaClient();
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-// Export the Prisma client instance for usage in other files
-export default prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
